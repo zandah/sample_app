0ad6fbe9dc3ca49d43a0cc8ff5b0c9cf6129e60a
Alexander Perry <alexander_perry@me.com>
2013-01-20 14:25:39 -0500

    Page titles added

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,6 +6,12 @@ describe "Home page" do
 		visit '/static_pages/home'
 		page.should have_content('Sample App')
 	end
+
+	it "should have the right title" do
+		visit '/static_pages/home'
+		page.should have_selector('title',
+				:text => "Ruby on Rails Tutorial Sample App | Home")
+	end
 end
 
 describe "Help page" do
@@ -14,6 +20,12 @@ describe "Help page" do
 		visit '/static_pages/help'
 		page.should have_content('Help')
 	end
+
+	it "should have the right title" do
+		visit '/static_pages/help'
+		page.should have_selector('title',
+				:text => "Ruby on Rails Tutorial Sample App | Help")
+	end
 end
 
 describe "About Us" do
@@ -21,5 +33,11 @@ describe "About Us" do
 	it "should have the content 'About Us'" do
 		visit '/static_pages/about'
 		page.should have_content('About Us')
+	end
+
+	it "should have the right title" do
+		visit '/static_pages/about'
+		page.should have_selector('title',
+				:text => "Ruby on Rails Tutorial Sample App | About Us")
 	end
 end
